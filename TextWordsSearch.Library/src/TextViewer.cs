04b8f13b05ae9530cfd9d4bc255068c5ad226c1b
Denis Rezepov <me@dar920910.ru>
2023-09-12 21:21:10 +0300

    Deleted the WordCalculator class.

--- TextWordsSearch.Library/src/TextViewer.cs
+++ TextWordsSearch.Library/src/TextViewer.cs
@@ -33,9 +33,9 @@
 			OutputAllDataFromTextFile(dataStringsFromTextFile);
 
 			string[] detectedTextWords = FindAllTextWords(dataStringsFromTextFile);
-			OutputWords(detectedTextWords);
+			OutputDetectedWords(detectedTextWords);
 
-			SuperWord resultSuperWord = new WordCalculator().CalculateSuperWord(detectedTextWords);
+			(string Value, int Count) resultSuperWord = FindSuperWord(detectedTextWords);
 			OutputSuperWord(resultSuperWord);
 		}
 
@@ -77,6 +77,37 @@
 			return subString;
 		}
 
+		private static (string Value, int Count) FindSuperWord(string[] detectedTextWords)
+		{
+			string superWordValue = string.Empty;
+			int superWordCount = 0;
+
+			int[] wordsNumbers = new int[detectedTextWords.Length];
+
+			for (int i = 0; i < detectedTextWords.Length; i++)
+			{
+				int currentWordCount = 0;
+
+				foreach (var word in detectedTextWords)
+				{
+					if (word.Equals(detectedTextWords[i]))
+					{
+						currentWordCount++;
+					}
+				}
+
+				wordsNumbers[i] = currentWordCount;
+
+				if (currentWordCount > superWordCount)
+				{
+					superWordValue = detectedTextWords[i].ToUpper();
+					superWordCount = currentWordCount;
+				}
+			}
+
+			return (Value: superWordValue, Count: superWordCount);
+		}
+
 
 		private void OutputAllDataFromTextFile(string[] dataStringsFromTextFile)
 		{
@@ -89,7 +120,7 @@
 			}
 		}
 
-		private void OutputWords(string[] detectedTextWords)
+		private void OutputDetectedWords(string[] detectedTextWords)
 		{
 			_LogWriter.WriteLine(GetOutputTextDelimiter());
 			_LogWriter.WriteLine($"\n[{DateTime.Now}] INFO: Detected Text Words in the Loaded File:\n");
@@ -102,21 +133,10 @@
 			_LogWriter.WriteLine();
 		}
 
-		private void OutputCountWords(string[] detectedTextWords, int[] detectedTextWordsCount)
+		private void OutputSuperWord((string Value, int Count) super)
 		{
 			_LogWriter.WriteLine(GetOutputTextDelimiter());
-			_LogWriter.WriteLine($"\n[{DateTime.Now}] INFO: Count of Text Words from the Loaded File:\n");
-
-			for(int i = 0; i < detectedTextWords.Length; i++)
-			{
-				_LogWriter.WriteLine($"   Word: {detectedTextWords[i].ToUpper()} | Count: {detectedTextWordsCount[i]}");
-			}
-		}
-
-		private void OutputSuperWord(SuperWord super)
-		{
-			_LogWriter.WriteLine(GetOutputTextDelimiter());
-			_LogWriter.WriteLine($"\n[{DateTime.Now}] INFO: SuperWord: '{super.curWord}' | Count: {super.curCount}.\n");
+			_LogWriter.WriteLine($"\n[{DateTime.Now}] INFO: SuperWord: '{super.Value}' | Count: {super.Count}.\n");
 		}
 
 		private static string GetOutputTextDelimiter()
@@ -125,87 +145,6 @@
 			const char delimiterChar = '=';
 
 			return new string(delimiterChar, delimiterSize);
-		}
-	}
-
-
-	/// <summary>
-	/// dar920910: Структура, содержащая название и количество вхождений "супер-слова".
-	/// </summary>
-	public struct SuperWord
-	{
-		public string curWord; // название "супер-слова"
-		public int curCount; // количество вхождений "супер-слова"
-	}
-
-	/// <summary>
-	/// dar920910: Класс WordCalculator содержит алгоритмы, предназначенные для подсчета
-	/// найденных слов в целевом текстовом файле.
-	/// </summary>
-	public class WordCalculator
-	{
-		// Объявляем переменную типа структуры SuperWord для хранения "супер-слова":
-		SuperWord currentSuperWord;
-
-		/// <summary>
-		/// dar920910: Переопределенный конструктор класса WordCalculator,
-		/// выполняющий инициализацию полей структуры, в которую будет сохранен результат.
-		/// </summary>
-		public WordCalculator()
-		{
-			// Инициализируем поля структуры currentSuperWord значениями по умолчанию:
-
-			currentSuperWord.curWord = string.Empty;
-			currentSuperWord.curCount = 0;
-		}
-
-		/// <summary>
-		/// dar920910: Метод CalculateSuperWord() принимает в качестве аргумента массив слов
-		/// целевого текста и возвращает самое часто встречающееся слово в этом массиве.
-		/// </summary>
-		/// <param name="arrWords">Целевой массив, состоящий из слов.</param>
-		/// <returns>Метод возвращает самое часто встречающееся слово в разбираемом
-		/// массиве слов.</returns>
-		public SuperWord CalculateSuperWord(string[] arrWords)
-		{		
-			// Объявляем целочисленный массив размерностью массива слов
-			// Этот массив будет хранить информацию о количестве вхождений слов в тексте.
-			int[] arrCount = new int[arrWords.Length];
-
-			// Алгоритм подсчета, циклический обход массива слов, принимаемого параметром метода arrWords:
-
-			for (int i = 0; i < arrWords.Length; i++)
-			{
-				// Локальная переменная, используемая для подсчета числа вхождений текущего слова в массиве слов:
-				int count = 0; // инициализируем count нулем
-				
-				// Запускаем внутренний цикл для поиска одинаковых слов в массиве слов:
-				foreach (var word in arrWords)
-				{
-					// Текущее слово эквивалентно проверяемому ?
-
-					//if (word == arrWords[i]) // более простой и понятный вариант
-					if (word.Equals(arrWords[i])) // более корректный вариант
-						count++; // инкрементируем count, ведь найдено еще одно вхождение слова!
-				}
-
-				// Инициализируем текущий элемент массива arrCount только что вычисленным значением: count:
-				arrCount[i] = count;
-
-				// Заключительная проверка:
-				// Количество вхождений count текущего слова больше, чем текущее значение поля curCount структуры currentSuperWord ?
-				if (count > currentSuperWord.curCount)
-				{
-					// Присваиваем значения полям структуры currentSuperWord:
-
-					// Определение количества вхождений часто встречающегося слова:
-					currentSuperWord.curCount = count; // похоже, что count достойно зваться "супер-словом" ...
-					// Определение самого часто встречающегося слова (преобразуем к верхнему регистру согласно заданию):
-					currentSuperWord.curWord = arrWords[i].ToUpper(); // текущее слово становится обладателем титула "супер-слово"!!!
-				}
-			}
-
-			return currentSuperWord;
 		}
 	}
 }
