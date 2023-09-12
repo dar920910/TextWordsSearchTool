8dbe7eedb39100ae315e84f649ac0689cccd4982
Denis Rezepov <me@dar920910.ru>
2023-09-12 19:45:35 +0300

    Deleted the FileScanner class.

--- TextWordsSearch.Library/src/TextViewer.cs
+++ TextWordsSearch.Library/src/TextViewer.cs
@@ -30,7 +30,7 @@
 		{
 			_LogWriter.WriteLine($"[{DateTime.Now}] INFO: Start reading data from the text file ...");
 
-			string[] arrTextLoremIpsum = new FileScanner().ReadText_LoremIpsum(_FilePath);
+			string[] arrTextLoremIpsum = File.ReadAllLines(_FilePath);
 
 			FileResult.OutputTextLoremIpsum(arrTextLoremIpsum);
 
@@ -41,39 +41,6 @@
 			FileResult.OutputSuperWord(resultSuperWord);
 		}
 	}
-
-
-	/// <summary>
-	/// dar920910: Класс FileScanner представляет собой реализацию устройства,
-	/// обеспечивающего обработку целевого текстового файла.
-	/// </summary>
-	public class FileScanner
-	{
-		/// <summary>
-		/// dar920910: Метод ReadText_LoremIpsum() выполняет поиск и чтение
-		/// целевого текстового файла, содержащего текст-"рыбу" Lorem Ipsum. 
-		/// </summary>
-		/// <param name="fileLoremIpsum">Путь к целевому файлу, содержащему текст-"рыбу"
-		/// Lorem Ipsum.</param>
-		/// <returns>Метод возвращает строковый массив, содержащий данные,
-		/// прочитанные из целевого файла.</returns>
-		public string[] ReadText_LoremIpsum(string fileLoremIpsum)
-		{
-			/* Чтение целевого файла:
-			 * 1. Объявление строкового массива strTextLoremIpsum для сохранения
-			 * считываемых данных.
-			 * 2. Вызов метода ReadAllLines() с аргументом, представляющим
-			 * собой имя целевого файла.
-			 * 3. Инициализация массива strTextLoremIpsum возвращаемым значением,
-			 * полученным при вызове метода ReadAllLines().
-			 */ 
-			
-			string[] strTextLoremIpsum = File.ReadAllLines(fileLoremIpsum);
-
-			return strTextLoremIpsum;
-		}
-	}
-
 
 	/// <summary>
 	/// dar920910: Класс FileResult выполняет функции вывода результатов
