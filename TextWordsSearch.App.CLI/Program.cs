6a7ad4f66dead4d7a841382ae401e0dee76c5507
Denis Rezepov <me@dar920910.ru>
2023-10-05 14:16:06 +0300

    The TextWordsCounter class was embedded to the TextWordsViewer class and project's console application.

--- TextWordsSearch.App.CLI/Program.cs
+++ TextWordsSearch.App.CLI/Program.cs
@@ -14,8 +14,9 @@ try
     {
         Console.WriteLine($"[INFO] Detected the custom file: {customTextFile}");
 
-        TextViewer textViewer = new (customTextFile);
-        textViewer.ReadFileContent();
+        TextWordsViewer viewer = new (customTextFile);
+        ConsoleKeyInfo selectionKeyInfo = ReadCustomSelectionKey();
+        ExecuteAnalysisTextContent(selectionKeyInfo, viewer);
     }
     else
     {
@@ -25,4 +26,121 @@ try
 catch (IndexOutOfRangeException)
 {
     Console.WriteLine($"[ERROR] You should pass a custom text file as the one argument to the program.");
+}
+
+ConsoleKeyInfo ReadCustomSelectionKey()
+{
+    Console.WriteLine("Select an action to analyze the content of this text file:");
+    Console.WriteLine("-) 'A' - Show text words with the maximum entry count in this text file");
+    Console.WriteLine("-) 'B' - Show text words with the minimum entry count in this text file");
+    Console.WriteLine("-) 'C' - Show all detected text words ordered by ascending");
+    Console.WriteLine("-) 'D' - Show all detected text words ordered by descending");
+
+    ConsoleKey[] possibleKeys = new[]
+    {
+        ConsoleKey.A,
+        ConsoleKey.B,
+        ConsoleKey.C,
+        ConsoleKey.D,
+    };
+
+    return ReadSelectionKey(possibleKeys);
+}
+
+ConsoleKeyInfo ReadSelectionKey(ConsoleKey[] availableKeys)
+{
+    do
+    {
+        Console.Write("Press a selection key: ");
+        ConsoleKeyInfo userKey = Console.ReadKey();
+        Console.WriteLine();
+
+        if (availableKeys.Contains(userKey.Key))
+        {
+            return userKey;
+        }
+        else
+        {
+            Console.WriteLine("ERROR! Please select a possible key!");
+        }
+    }
+    while (true);
+}
+
+void ExecuteAnalysisTextContent(ConsoleKeyInfo actionSelectionKeyInfo, TextWordsViewer viewer)
+{
+    switch (actionSelectionKeyInfo.Key)
+    {
+        case ConsoleKey.A:
+            ShowWordsWithMaximumEntryCount(viewer);
+            break;
+        case ConsoleKey.B:
+            ShowWordsWithMinimumEntryCount(viewer);
+            break;
+        case ConsoleKey.C:
+            ShowDetectedWordsWithEntryCountOrderedByAscending(viewer);
+            break;
+        case ConsoleKey.D:
+            ShowDetectedWordsWithEntryCountOrderedByDescending(viewer);
+            break;
+        default:
+            break;
+    }
+}
+
+void ShowWordsWithMaximumEntryCount(TextWordsViewer viewer)
+{
+    KeyValuePair<uint, List<string>> wordsWithCount = viewer.WordsWithMaximumEntryCount;
+    PrintWordsWithEntryCount(wordsWithCount, "Maximum Count of Words");
+}
+
+void ShowWordsWithMinimumEntryCount(TextWordsViewer viewer)
+{
+    KeyValuePair<uint, List<string>> wordsWithCount = viewer.WordsWithMinimumEntryCount;
+    PrintWordsWithEntryCount(wordsWithCount, "Minimum Count of Words");
+}
+
+void ShowDetectedWordsWithEntryCountOrderedByAscending(TextWordsViewer viewer)
+{
+    List<KeyValuePair<uint, List<string>>> detectedWords = viewer.TextWordsOrderedByAscending;
+    PrintDetectedWordsWithEntryCount(detectedWords, "Detected Text Words Ordered By Ascending");
+}
+
+void ShowDetectedWordsWithEntryCountOrderedByDescending(TextWordsViewer viewer)
+{
+    List<KeyValuePair<uint, List<string>>> detectedWords = viewer.TextWordsOrderedByDescending;
+    PrintDetectedWordsWithEntryCount(detectedWords, "Detected Text Words Ordered By Descending");
+}
+
+static void PrintWordsWithEntryCount(KeyValuePair<uint, List<string>> wordsWithCount, string message)
+{
+    Console.WriteLine($"{message}: {wordsWithCount.Key} times in this text file.");
+    Console.WriteLine($"These text words are following:");
+
+    foreach (string word in wordsWithCount.Value)
+    {
+        Console.WriteLine(word);
+    }
+
+    Console.WriteLine();
+}
+
+static void PrintDetectedWordsWithEntryCount(List<KeyValuePair<uint, List<string>>> detectedWords, string message)
+{
+    Console.WriteLine($"{message}:");
+    Console.WriteLine();
+
+    foreach (KeyValuePair<uint, List<string>> pair in detectedWords)
+    {
+        Console.Write($"EntryCount = {pair.Key}: ");
+
+        foreach (string word in pair.Value)
+        {
+            Console.Write($"'{word}', ");
+        }
+
+        Console.WriteLine();
+    }
+
+    Console.WriteLine();
 }
---
+++ TextWordsSearch.Library/src/TextWordsViewer.cs
@@ -0,0 +1,90 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="TextWordsViewer.cs" company="Demo Projects Workshop">
+//     Copyright (c) Demo Projects Workshop. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+#pragma warning disable SA1600 // ElementsMustBeDocumented
+
+namespace TextWordsSearch.Library;
+
+public class TextWordsViewer
+{
+    private readonly string textFilePath;
+    private readonly StreamWriter writer;
+    private readonly Dictionary<string, uint> detectedTextWords;
+
+    public TextWordsViewer(string dataTextFilePath)
+    {
+        this.writer = new StreamWriter(
+            Path.Combine(Directory.GetCurrentDirectory(), $"results.log"));
+
+        if (File.Exists(dataTextFilePath))
+        {
+            this.writer.WriteLine($"[{DateTime.Now}] SUCCESS: File {dataTextFilePath} was successfully detected.");
+            this.textFilePath = dataTextFilePath;
+        }
+        else
+        {
+            this.writer.WriteLine($"[{DateTime.Now}] ERROR: File {dataTextFilePath} was not detected!");
+        }
+
+        this.writer.WriteLine($"[{DateTime.Now}] INFO: Start reading data from the text file ...");
+
+        string currentTextFileContent = File.ReadAllText(this.textFilePath);
+        string[] currentTextWords = TextWordsCounter.GetWordsArrayFromTextContent(currentTextFileContent);
+        this.detectedTextWords = TextWordsCounter.GetWordsDictionaryFromWordsArray(currentTextWords);
+
+        this.writer.Close();
+    }
+
+    public KeyValuePair<uint, List<string>> WordsWithMaximumEntryCount =>
+        TextWordsCounter.GetInfoAboutMaximumEntryCountOfWordsInText(this.detectedTextWords);
+
+    public KeyValuePair<uint, List<string>> WordsWithMinimumEntryCount =>
+        TextWordsCounter.GetInfoAboutMinimumEntryCountOfWordsInText(this.detectedTextWords);
+
+    public List<KeyValuePair<uint, List<string>>> TextWordsOrderedByAscending =>
+        TextWordsCounter.GetSortedWordsByAscending(this.detectedTextWords);
+
+    public List<KeyValuePair<uint, List<string>>> TextWordsOrderedByDescending =>
+        TextWordsCounter.GetSortedWordsByDescending(this.detectedTextWords);
+
+    private static string GetOutputTextDelimiter()
+    {
+        const byte delimiterSize = 100;
+        const char delimiterChar = '=';
+        return new string(delimiterChar, delimiterSize);
+    }
+
+    private void OutputAllDataFromTextFile(string[] dataStringsFromTextFile)
+    {
+        this.writer.WriteLine(GetOutputTextDelimiter());
+        this.writer.WriteLine($"\n[{DateTime.Now}] INFO: Text Data from the Loaded File:\n");
+
+        foreach (string dataString in dataStringsFromTextFile)
+        {
+            this.writer.WriteLine(dataString);
+        }
+    }
+
+    private void OutputDetectedWords(string[] detectedTextWords)
+    {
+        this.writer.WriteLine(GetOutputTextDelimiter());
+        this.writer.WriteLine($"\n[{DateTime.Now}] INFO: Detected Text Words in the Loaded File:\n");
+
+        for (int i = 0; i < detectedTextWords.Length; i++)
+        {
+            this.writer.WriteLine($"   i = {i}: {detectedTextWords[i]}");
+        }
+
+        this.writer.WriteLine();
+    }
+
+    private void OutputSuperWord((string Value, int Count) super)
+    {
+        this.writer.WriteLine(GetOutputTextDelimiter());
+        string outputResult = $"\n[{DateTime.Now}] INFO: SuperWord: '{super.Value}' | Count: {super.Count}.\n";
+        this.writer.WriteLine(outputResult);
+    }
+}
